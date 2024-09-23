c8f13047f6386cec57ee920dd36392990fed8a00
Olivares10Abad <olivaresabadfco10@gmail.com>
2024-09-23 17:23:56 -0600

    Configuuración de dependencias y credenciales

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const pool = new Pool({
     user: process.env.PGUSER,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
+    port: process.env.PGPORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Middleware para parsear JSON
